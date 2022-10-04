af4a12053bc8817a80484efaeb11a8fcf4c3e292
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-10-04 22:16:31 +0200

    item cards

--- pages/news.tsx
+++ pages/news.tsx
@@ -1,10 +1,20 @@
+import ItemCard from "./itemCard";
+
 const News = function () {
   return (
     <>
       <div className="container mx-auto text-center">
-        <h1 className="mt-12 text-gray-800 text-3xl tracking-wider uppercase">
+        <h1 className="mt-12 text-gray-800 text-3xl tracking-wider uppercase mb-12">
           Nyheter!
         </h1>
+        <section className="flex flex-row flex-wrap">
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </section>
       </div>
     </>
   );
